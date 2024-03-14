1abe467ea7a049e713b0c869666477de723770dd
Jarco van Roest <jarco@elnino.tech>
2024-03-14 23:38:58 +0100

    feat(config): move projects into config.json

--- cloudflare_dns.py
+++ cloudflare_dns.py
@@ -11,6 +11,7 @@ with open('config.json', 'r') as config_file:
     EMAIL = config["CLOUDFLARE_EMAIL"]
     NTFY_HOST = config["NTFY_HOST"]
     NTFY_PATH = config["NTFY_PATH"]
+    PROJECTS = config["PROJECTS"]
 
 
 @dataclass
@@ -72,6 +73,18 @@ def update_all_records(sites: List[CloudflareDNS], ip):
         print(f"Updated {site.name} DNS record:", result)
 
 
+def parse_projects():
+    sites = []
+    for project in PROJECTS:
+        site = CloudflareDNS(
+            zone_id=project["zone_id"],
+            dns_record_id=project["dns_record_id"],
+            name=project["name"],
+            proxied=project["proxied"]
+        )
+        sites.append(site)
+    return sites
+
 def main():
     external_ip = get_external_ip()
     conn = sqlite3.connect('cloudflare.db')
@@ -80,26 +93,15 @@ def main():
         'CREATE TABLE IF NOT EXISTS external_ip (ip TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
 
     res = c.execute('SELECT * FROM external_ip ORDER BY ROWID DESC').fetchone()
-    if True or res is None or res[0] != external_ip:
+    if res is None or res[0] != external_ip:
         c.execute('INSERT INTO external_ip (ip) VALUES (?)', (external_ip,))
         conn.commit()
 
         msg = "External IP changed, updating Cloudflare DNS settings"
         send_notification(msg)
 
-        site_9t9 = CloudflareDNS(
-            zone_id='85fa5a3e1d87143f27dff1ce75096d06',
-            dns_record_id='e9047de14c4bcccc1e29ff046a52240a',
-            name='9t9.tech'
-        )
-
-        site_rait = CloudflareDNS(
-            zone_id='82903fcace7476d57a6e94a7c8b8bbff',
-            dns_record_id='0ed8b6f09a3ff027de29eb9980d4f41a',
-            name='rait.tech',
-            proxied=True
-        )
-        update_all_records([site_9t9, site_rait], external_ip)
+        parsed_projects = parse_projects()
+        update_all_records(parsed_projects, external_ip)
     else:
         print("IP address has not changed, skipping update")
 
